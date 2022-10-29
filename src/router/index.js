aac1935f582fd5a814050cdd1fb76d2ae31fba0f
cuhong <hongcoilhouse@gmail.com>
2022-10-30 03:56:41 +0900

    add redirects

--- src/router/index.js
+++ src/router/index.js
@@ -1,40 +1,48 @@
 import { createWebHistory, createRouter } from "vue-router";
+import {verifyToken} from "../services/auth.js";
 
 const routes = [
     {
         path: "/",
         name: "CompanyHome",
         component: () => import("../components/Home.vue"),
+        meta: {requireAuth: null}
     },
     {
         path: "/policy/privacy",
         name: "PolicyPrivacy",
         component: () => import("../components/policy/Privacy.vue"),
+        meta: {requireAuth: null}
     },
     {
         path: "/policy/service",
         name: "PolicyService",
         component: () => import("../components/policy/Service.vue"),
+        meta: {requireAuth: null}
     },
     {
         path: "/login",
         name: "Login",
         component: () => import("../components/auth/Login.vue"),
+        meta: {requireAuth: false}
     },
     {
         path: "/auth/kakao",
         name: "KakaoCallback",
         component: () => import("../components/auth/KakaoCallback.vue"),
+        meta: {requireAuth: false}
     },
     {
         path: "/company/receipt",
         name: "CompanyReceipt",
         component: () => import("../components/receipt/Receipt.vue"),
+        meta: {requireAuth: true}
     },
     {
         path: "/company/receipt/personal",
         name: "CompanyReceiptPersonal",
         component: () => import("../components/receipt/Personal.vue"),
+        meta: {requireAuth: true}
     },
 ];
 
@@ -42,5 +50,28 @@ const router = createRouter({
     history: createWebHistory(),
     routes,
 });
+
+router.beforeEach((to, from, next) => {
+    const requireAuth = to.meta.requireAuth
+    const authStatus = verifyToken()
+    if (requireAuth === null) {
+        next();
+    } else if (requireAuth === true) {
+        // 로그인이 필요한 페이지
+        if (authStatus) {
+            next();
+        } else {
+            next({name: "Login"});
+        }
+    } else {
+        if (authStatus) {
+            // 로그인이 되어 있는 경우 접수 화면으로 이동
+            next({name: "CompanyReceipt"});
+        } else {
+            // 로그인이 안되어 있다면 페이지를 정상적으로 표시
+            next();
+        }
+    }
+})
 
 export default router;
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,3 +1,5 @@
+import urlJoin from "url-join";
+import axios from "axios";
 const setToken = (accessToken, refreshToken) => {
   localStorage.setItem('accessToken', accessToken);
   localStorage.setItem('refreshToken', refreshToken);
@@ -10,9 +12,42 @@ const getToken = () => {
     }
 }
 
-const verifyToken = () => {
+const clearToken = () => {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+}
+const verifyToken = async () => {
     const tokens = getToken();
-
+    if (tokens.accessToken === null || tokens.refreshToken === null) {
+        clearToken()
+        return null
+    }
+    try {
+        const verifyResponse = await axios.post(
+            urlJoin(process.env.VITE_BACKEND_HOST, "/account/token/verify/"),
+            {token: tokens.accessToken},
+            {headers: {'Content-Type': 'application/json'}},
+            {validateStatus: (status) => [401, 200].indexOf(status) !== -1}
+        )
+        if (verifyResponse.status === 200) {
+            return true
+        }
+        const refreshResponse = await axios.post(
+            urlJoin(process.env.VITE_BACKEND_HOST, "/account/token/refresh/"),
+            {refresh: tokens.refreshToken},
+            {headers: {'Content-Type': 'application/json'}},
+            {validateStatus: (status) => [401, 200].indexOf(status) !== -1}
+        )
+        if (refreshResponse.status === 401) {
+            clearToken();
+            return false
+        }
+        setToken(refreshResponse.data.access, tokens.refreshToken)
+        return true
+    } catch (e) {
+        clearToken();
+        return false
+    }
 }
 
 export {
